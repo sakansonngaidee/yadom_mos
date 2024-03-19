6be20c857266a495c464c4bfcf65741129132df0
DaiNoZuk <131583630+DaiNoZuk@users.noreply.github.com>
2024-03-19 16:06:04 +0700

    Calendar

--- src/assets/components/pages/Home.jsx
+++ src/assets/components/pages/Home.jsx
@@ -1,7 +1,31 @@
-export const Home = () => {
-    return (
-        <div>
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import { format } from "date-fns";
+import "react-datepicker/dist/react-datepicker.css";
 
-        </div>
-    )
-}
+export const Home = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [isOpen, setIsOpen] = useState(false);
+  const handleChange = (e) => {
+    setIsOpen(!isOpen);
+    setStartDate(e);
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div className="grid w-60" >
+      <button className="bg-[#93c5fd]" onClick={handleClick}>
+        {format(startDate, "dd-MMM-yyyy")}
+      </button>
+      {isOpen && (
+        <DatePicker
+          selected={startDate}
+          onChange={handleChange}
+          inline
+        />
+      )}
+    </div>
+  );
+};
